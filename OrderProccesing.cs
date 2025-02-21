46bf765ebceb8acf3e12c9c76dff6ea446fe23bf
Harsha <harshanp603@gmail.com>
2025-02-21 09:30:03 +0530

    Adding Order Processing

---
+++ OrderProccesing.cs
@@ -0,0 +1,36 @@
+﻿// Online Order Procesing System using Asynchronous Programming
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Orders
+{
+    class OrderProcessing
+    {
+        public static async Task Main(String[] args)
+        {
+            
+            string orderName;
+
+            // Taking orderName as an input from the user
+            Console.Write("Hello User, Please enter the Order Name : ");
+            orderName = Console.ReadLine();
+
+            //var processing = ProcessingOrder();
+            //await Task.WhenAll(processing);
+            await ProcessingOrder();
+            // Displaying the order status as ready
+            Console.WriteLine($"Order for {orderName} is ready");
+        }
+
+        // Asynchronous Method which Processes the Order for 3 seconds
+        public static async Task ProcessingOrder()
+        {
+            // Processing the order in 3seconds
+            Console.WriteLine("Processing Order...");
+            await Task.Delay(3000);
+
+        }
+    }
+}
